d5cbfab2127281f24a74a0eb8264279bb35fa987
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2026-06-25 12:57:38 +0600

    Add create.blade.php with form enctype and validation styling

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,3 +1,83 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="bg-dark text-white text-center py-3 mb-4">
+        <h1 class="h2">Laravel CRUD</h1>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="d-flex justify-content-end p-0 mt-3">
+                <a href="" class="btn btn-dark">Create</a>
+            </div>
+            <div class="card p-0 mt-3">
+                <div class="card-header bg-dark text-white">
+                    <h2 class="h4">Create Product</h2>
+                </div>
+                <div class="card-body shadow-lg">
+                    <form action="{{ route('products.store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" value="{{ old('name') }}" class="form-control @error('name') is-invalid @enderror"
+                                id="name" name="name" placeholder="Name">
+                            @error('name')
+                                <p class="invalid-feedback">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Image</label>
+                            <input type="file" class="form-control @error('image') is-invalid @enderror"
+                                id="image" name="image">
+                            @error('image')
+                                <p class="invalid-feedback">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="sku" class="form-label">SKU</label>
+                            <input type="text" value="{{ old('sku') }}" class="form-control @error('sku') is-invalid @enderror" id="sku"
+                                name="sku" placeholder="SKU">
+                            @error('sku')
+                                <p class="invalid-feedback">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Price</label>
+                            <input type="text" value="{{ old('price') }}" class="form-control @error('price') is-invalid @enderror"
+                                id="price" name="price" placeholder="Price">
+                            @error('price')
+                                <p class="invalid-feedback">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="status" class="form-label">Status</label>
+                            <select name="status" id="status" class="form-select">
+                                <option value="Active">Active</option>
+                                <option value="Inactive">Inactive</option>
+                            </select>
+                        </div>
+                        <button class="btn btn-dark">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
